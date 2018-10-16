bc55737c86aaffd6f927f3e9941e061601d50b76
tarunguptap <tarunguptap@yahoo.com>
2018-10-16 15:39:17 +0530

    Update Auditable.java

--- src/com/spring/domain/Auditable.java
+++ src/com/spring/domain/Auditable.java
@@ -4,7 +4,7 @@ public interface Auditable {
 	/**
 	 * Instances must always return a non-null instance of AuditInfo
 	 * 
-	 * @hibernate.component class="com.flipswap.domain.AuditInfo"
+	 *
 	 */
 	public AuditInfo getAuditInfo();
 }
